810062efb2a3b2769864f5615fd6130721756b27
qwzky1 <100063507+qwzky1@users.noreply.github.com>
2025-08-10 20:38:13 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
-// build.gradle.kts - la nivel de proiect (root)
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
